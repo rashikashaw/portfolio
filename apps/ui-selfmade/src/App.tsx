2813b774f3825dcfc429039c180adb53807364a2
Rashika Shaw <rashikashaw97@gmail.com>
2023-02-06 20:46:10 -0800

    ui-selfmade init

---
+++ apps/ui-selfmade/src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <>Hi</>
+  );
+};
+
+export default App
---
+++ apps/ui-selfmade/src/css-utils/themes.ts
@@ -0,0 +1,24 @@
+
+const lightMode = {
+  colors: {
+    "gray-20": "#F8F4EB",
+    "gray-50": "#EFE6E6",
+    "gray-100": "#DFCCCC",
+    "gray-500": "#5E0000",
+    "primary-100": "#FFE1E0",
+    "primary-300": "#FFA6A3",
+    "primary-500": "#FF6B66",
+    "secondary-400": "#FFCD5B",
+    "secondary-500": "#FFC132",
+  },
+  background: {
+    
+  },
+  fontFamily: {
+    dmsans: ["DM Sans", "sans-serif"],
+    montserrat: ["Montserrat", "sans-serif"],
+  },
+  fontSize: {
+    
+  }
+};
