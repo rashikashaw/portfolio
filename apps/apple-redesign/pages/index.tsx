126514d611c282e9219727dfb078bcd23decf7e8
Rashika Shaw <rashikashaw97@gmail.com>
2023-02-08 20:24:46 -0800

    apple redesign ui and api setup

---
+++ apps/apple-redesign/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import React from 'react';
+import { Header } from '../components/Header';
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+    </div>
+  )
+}
+
+export default Home
