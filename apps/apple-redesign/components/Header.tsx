1d6b70fa5a7deab7e00e9e0ee372c7f6c134924e
Rashika Shaw <rashikashaw97@gmail.com>
2023-02-08 20:24:46 -0800

    apple redesign ui and api setup

---
+++ apps/apple-redesign/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Image from "next/image";
+
+export const Header = () => {
+  return <header>
+    <div>
+      <Image alt="" src="https://rb.gy/vsvv2o" fill style={{ objectFit: 'contain' }} /> 
+    </div>
+  </header>
+}
