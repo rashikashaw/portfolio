122cf26db3e069d5766b3db9ebf7d8c8f0c0ff9d
Rashika Shaw <rashikashaw97@gmail.com>
2023-02-09 12:23:02 -0800

    Navbar Component

--- apps/apple-redesign/pages/index.tsx
+++ apps/apple-redesign/pages/index.tsx
@@ -1,16 +1,16 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import React from 'react';
-import { Header } from '../components/Header';
+import { Navbar } from '../components/Navbar';
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
+      <Navbar />
     </div>
   )
 }
