16c3d6b01f0a45dab543bbe66c688d7ead43ca9a
Rashika Shaw <rashikashaw97@gmail.com>
2023-02-12 14:04:22 -0800

    api sanity

--- apps/api-apple-redesign/sanity.cli.ts
+++ apps/api-apple-redesign/sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'hzm21fbv',
+    projectId: 'jtjlvev0',
     dataset: 'production'
   }
 })
--- apps/apple-redesign/pages/index.tsx
+++ apps/apple-redesign/pages/index.tsx
@@ -1,10 +1,14 @@
-import { Button } from '@portfolio/button';
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 import Head from 'next/head';
 import React from 'react';
 import { Landing } from '../components/Landing';
 import { Navbar } from '../components/Navbar';
+import { fetchCategories } from "../utils/fetchCategories";
+import { Tab } from "@headlessui/react";
 
+type Props = {
+  categories: Category[];
+}
 const Home: NextPage = () => {
   return (
     <div>
@@ -13,12 +17,51 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <Button label={'Test'} onClick={() => {}} />
       <main className="relative h-[200vh] bg-[#E7ECEE]">
         <Landing />
       </main>
+      <section className='relative z-40 -mt-[100vh] min-h-screen bg-[#141414]'>
+        <div className='justify-center items-center space-y-10 py-16'>
+          <h1 className="text-center text-4xl font-medium tracking-wide text-white md:text-5xl">
+            New Promo
+          </h1>
+          <Tab.Group>
+            <Tab.List className="flex justify-center">
+              {/* {categories.map((category) => (
+                <Tab
+                  key={category._id}
+                  id={category._id}
+                  className={({ selected }) =>
+                    `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
+                      selected
+                        ? "borderGradient bg-[#35383C] text-white"
+                        : "border-b-2 border-[#35383C] text-[#747474]"
+                    }`
+                  }
+                >
+                  {category.title}
+                </Tab>
+              ))} */}
+            </Tab.List>
+            {/* <Tab.Panels className="mx-auto max-w-fit pt-10 pb-24 sm:px-4">
+              <Tab.Panel className="tabPanel">{showProducts(0)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(1)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(2)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(3)}</Tab.Panel>
+            </Tab.Panels> */}
+          </Tab.Group>
+        </div>
+      </section>
     </div>
   );
 };
 
 export default Home;
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const categories = await fetchCategories();
+  return {
+    props: {
+      categories,
+    },
+  };
+};
